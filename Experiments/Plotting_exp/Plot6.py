ecababe2d24c155781de138fb304902628167cd2
Bertille Follain <follain.bertille@gmail.com>
2025-02-04 21:00:17 +0100

    Update Plot6.py
    Update Plot6.py to include new datasets required by revision of the paper.

--- Experiments/Plotting_exp/Plot6.py
+++ Experiments/Plotting_exp/Plot6.py
@@ -5,10 +5,27 @@ import matplotlib.pyplot as plt
 
 # Load the results
 results = pickle.load(open('../../Experiments_results/Results/Experiment6.pkl', 'rb'))['results']
+results_extra_yolanda = pickle.load(open('../../Experiments_results/Results/Experiment6_extra_Yolanda.pkl', 'rb'))['results']
+results_extra_music = pickle.load(open('../../Experiments_results/Results/Experiment6_extra_geographical_origin_of_music.pkl', 'rb'))['results']
+results_extra_tecator = pickle.load(open('../../Experiments_results/Results/Experiment6_extra_tecator.pkl', 'rb'))['results']
+results_extra_pah = pickle.load(open('../../Experiments_results/Results/Experiment6_extra_pah.pkl', 'rb'))['results']
+results_extra_semeion = pickle.load(open('../../Experiments_results/Results/Experiment6_extra_semeion.pkl', 'rb'))['results']
+
+# Merge results
+for dataset in results_extra_yolanda:
+    results[dataset] = results_extra_yolanda[dataset]
+for dataset in results_extra_music:
+    results[dataset] = results_extra_music[dataset]
+for dataset in results_extra_tecator:
+    results[dataset] = results_extra_tecator[dataset]
+for dataset in results_extra_pah:
+    results[dataset] = results_extra_pah[dataset]
+for dataset in results_extra_semeion:
+    results[dataset] = results_extra_semeion[dataset]
 
 # Convert results to a DataFrame
 data = []
-for dataset, scores in results.items():
+for dataset, scores in sorted(results.items()):
     data.append([dataset + ', d=' + str(scores[1]), 'BKerNN, Concave_Feature', scores[0][0]])
     data.append([dataset + ', d=' + str(scores[1]), 'BKerNN, Concave_Variable', scores[0][1]])
     data.append([dataset + ', d=' + str(scores[1]), 'BKRR', scores[0][2]])
@@ -30,7 +47,9 @@ plt.xlabel('Dataset', fontsize=16)
 plt.ylabel(r'$R^2$'+ ' score on test set', fontsize=16)
 plt.ylim(0, 1)
 plt.xticks(rotation=70, ha='right')  # Rotate dataset labels for better readability
-plt.legend(title='Method', fontsize=10)
+
+plt.legend(bbox_to_anchor=(0, -0.05, 1, 0.2), loc="lower left",
+                mode="expand", borderaxespad=0, ncol=4, fontsize =12)
 plt.grid(True)
 plt.tight_layout()
 
